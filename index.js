e45381f556b7350aee88c03635d45e627171254d
Nishtha Sharma <166835080+nisharma123@users.noreply.github.com>
2025-01-12 21:09:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const db = new pg.Client({
   user: "postgres",
   host: "localhost",
   database: "Book-Notes",
-  password: "GingerNishtha",
+  password: "your password",
   port: 5432,
 });
 db.connect();
